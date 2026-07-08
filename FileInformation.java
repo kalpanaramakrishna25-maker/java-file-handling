199cb2b3af2acefc3d767cca3d281c4ad9fd4b3c
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-08 06:39:02 +0530

    Add FileInformation class to check file details

---
+++ FileInformation.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class FileInformation {
+    public static void main(String[] args) {
+        File file = new File("sample.txt");
+
+        if (file.exists()) {
+            System.out.println("File Name: " + file.getName());
+            System.out.println("File Path: " + file.getAbsolutePath());
+            System.out.println("Readable: " + file.canRead());
+            System.out.println("Writable: " + file.canWrite());
+            System.out.println("File Size: " + file.length() + " bytes");
+        } else {
+            System.out.println("File does not exist.");
+        }
+    }
+}
