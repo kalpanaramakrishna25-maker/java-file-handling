66c4ffcb25a5e1cd9d089d5d12f9d89583ab24f8
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-08 06:37:36 +0530

    Add ReadFileLineByLine class to read a file

---
+++ ReadFileLineByLine.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFileLineByLine {
+    public static void main(String[] args) {
+        try {
+            File file = new File("sample.txt");
+            Scanner reader = new Scanner(file);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+}
